bb5c113abacd0a81ce88ae6ec197e5b4a66dbbef
Shahil-Dev <alfee9006@gmail.com>
2026-09-01 19:59:20 +0600

    feat(app): connect centralized application routes to express app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import globalErrorHandler from './middlewares/globalErrorHandler';
-import notFound from './middlewares/notFound';
-
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
+import { AuthRoutes } from "./modules/Auth/auth.route";
+import router from "./routes";
 
 const app: Application = express();
 
@@ -11,12 +12,14 @@ app.use(express.json());
 app.use(cors());
 
 // Root Health Check Route
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
-    message: 'School Management ERP System API is Running Operational!',
+    message: "School Management ERP System API is Running Operational!",
   });
 });
+
+app.use("/api/v1", router);
 
 // Global Middlewares
 app.use(globalErrorHandler);
