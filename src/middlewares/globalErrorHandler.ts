0a3a2b9bbba73e469864e8aaff141d7bc2beb0e3
Shahil-Dev <alfee9006@gmail.com>
2026-09-02 09:02:54 +0600

    feat(error): format Zod validation errors clean in globalErrorHandler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,18 +1,32 @@
 import { ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong!';
+  let errorSources = [
+    {
+      path: '',
+      message: err.message || 'Something went wrong!',
+    },
+  ];
+
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'Validation Error';
+    errorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1] as string,
+        message: issue.message,
+      };
+    });
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    errorSources: [
-      {
-        path: '',
-        message,
-      },
-    ],
+    errorSources,
     stack: process.env.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
